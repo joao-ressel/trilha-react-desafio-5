2df18328e090cd8a6891998bec781668a245e6fe
João Carlos Ressel <joao.resselti@gmail.com>
2024-11-05 09:50:13 -0500

    corrigindo Links

--- components/CustomLink.js
+++ components/CustomLink.js
@@ -3,9 +3,7 @@ import Link from 'next/link';
 export default function CustomLink({ as, href, ...otherProps }) {
   return (
     <>
-      <Link as={as} href={href}>
-        <a {...otherProps} />
-      </Link>
+      <Link {...otherProps} as={as} href={href}></Link>
     </>
   );
 }
